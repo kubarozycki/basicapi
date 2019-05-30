d9888c40d773bb703a057fdcc3c93cc934409a60
jakub.rozycki <jakub.rozycki@ITM2757>
2019-05-30 13:03:26 +0200

    new package

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace BasicApi
+namespace webapp
 {
     public class Program
     {
